c0afc8952320eddc32a1b1e37137804ca9d6658c
Munkleson <alexwongsydney@gmail.com>
2024-07-30 15:52:56 +0900

    finished maaking the picture buttons select and deselect properly. Did not use :focus be cause I wanted to keep it selected even when not in focus until you either select another button or select the same one to deselect it

--- public/newFunctions.js
+++ public/newFunctions.js
@@ -1,51 +1,18 @@
-// const testButton = document.querySelector('.testButton');
-
-// testButton.addEventListener('click', () => {
-//     console.log('hi');
-// });
-let pictureButtonActive = false;
-let currentActiveButton;
-const pictureObject = {};
-
 const pictureButton = document.getElementsByClassName('smallPicture');
+let currentActiveButton;
 
 for (let index = 0; index < pictureButton.length; index++) {
-    // pictureObject[pictureButton[index]] = false;
-
     pictureButton[index].addEventListener('click', (event) => {
         const target = event.target;
-        const style = target.style;
-        // if (pictureObject[pictureButton[index]]){
-        //     style.transform = "scale(1.0, 1.0)"
-        //     style.transition = "0.3s";
-        //     pictureObject[pictureButton[index]] = false;
-        // } else {
-        //     style.transform = "scale(1.4, 1.4)";
-        //     style.transition = "0.3s";
-        //     currentActiveButton = target;
-        //     pictureObject[pictureButton[index]] = true;
-        // }
-
-        for (const iterator of pictureButton) {
-            if (iterator !== target){
-                iterator.style.transform = "scale(1.0, 1.0)";
-                iterator.style.boxShadow = "0 0 0";
-            // } else if (iterator === target && !pictureButtonActive){
-
-            // } else if (iterator === target && pictureButtonActive){
-            }
+        if (currentActiveButton !== target && currentActiveButton){
+            currentActiveButton.classList.replace('smallPictureFocus', 'smallPictureNotFocus');
         }
-
-        if (!pictureButtonActive){
-            style.transform = "scale(1.4, 1.4)";
-            style.boxShadow = "0 0 15px yellow";
-            pictureButtonActive = true;
-        } else if (pictureButtonActive){
-            style.transform = "scale(1.0, 1.0)";
-            style.boxShadow = "0 0 0";
-            pictureButtonActive = false;
+        currentActiveButton = target;
+        if (target.classList.contains('smallPictureNotFocus')){
+            target.classList.replace('smallPictureNotFocus', 'smallPictureFocus');
+        } else {
+            target.classList.replace('smallPictureFocus', 'smallPictureNotFocus');
         }
-
     });
 }
 
