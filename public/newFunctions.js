da09606fa85c9df55a0ec221d1b8bd41f78c9ef1
Munkleson <alexwongsydney@gmail.com>
2024-07-30 12:04:35 +0900

    tried adding shadow properties to picture buttons. Now it doesn't work as before, where I have to click twice to remove scaling from another button that is not being clicked, and scale up the current one

--- public/newFunctions.js
+++ public/newFunctions.js
@@ -10,13 +10,11 @@ const pictureObject = {};
 const pictureButton = document.getElementsByClassName('smallPicture');
 
 for (let index = 0; index < pictureButton.length; index++) {
-    pictureObject[pictureButton[index]] = false;
+    // pictureObject[pictureButton[index]] = false;
 
     pictureButton[index].addEventListener('click', (event) => {
         const target = event.target;
         const style = target.style;
-        let currentIndex = index;
-
         // if (pictureObject[pictureButton[index]]){
         //     style.transform = "scale(1.0, 1.0)"
         //     style.transition = "0.3s";
@@ -31,14 +29,23 @@ for (let index = 0; index < pictureButton.length; index++) {
         for (const iterator of pictureButton) {
             if (iterator !== target){
                 iterator.style.transform = "scale(1.0, 1.0)";
-            } else if (iterator === target && !pictureButtonActive){
-                style.transform = "scale(1.4, 1.4)";
-                pictureButtonActive = true;
-            } else if (iterator === target && pictureButtonActive){
-                style.transform = "scale(1.0, 1.0)";
-                pictureButtonActive = false;
+                iterator.style.boxShadow = "0 0 0";
+            // } else if (iterator === target && !pictureButtonActive){
+
+            // } else if (iterator === target && pictureButtonActive){
             }
         }
+
+        if (!pictureButtonActive){
+            style.transform = "scale(1.4, 1.4)";
+            style.boxShadow = "0 0 15px yellow";
+            pictureButtonActive = true;
+        } else if (pictureButtonActive){
+            style.transform = "scale(1.0, 1.0)";
+            style.boxShadow = "0 0 0";
+            pictureButtonActive = false;
+        }
+
     });
 }
 
