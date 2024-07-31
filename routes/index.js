ab9d7d921a922c76da0ea9ba508c80f589f22b6d
Munkleson <alexwongsydney@gmail.com>
2024-07-31 18:24:46 +0900

    My app wasn't saving the messages, and would reset each time the server restarts. Have not learnt how to do this yet, so I had to use chatGPT to do that process for now

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,32 @@
 // import { currentActiveButton, buttonChangeEvent  } from '../public/newFunctions';
 
-const messages = [
-    // {
-    //   text: "Hi there!",
-    //   user: "Amando",
-    //   added: new Date()
-    // },
-    // {
-    //   text: "Hello World!",
-    //   user: "Charles",
-    //   added: new Date()
-    // }
-  ];
+const fs = require('fs');
+const path = require('path');
+const messagesFilePath = path.join(__dirname, '../data/messages.json');
+
+const loadMessages = () => {
+  try {
+    const databuffer = fs.readFileSync(messagesFilePath);
+    const dataJSON = databuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+}
+
+const saveMessages = (messages) => {
+  const dataJSON = JSON.stringify(messages);
+  fs.writeFileSync(messagesFilePath, dataJSON);
+}
+
+let messages = loadMessages();
+
+const { text } = require('body-parser');
+const { Router } = require('express');
+
+const router = Router();
 
 const backgroundImageArray = ["https://i.pinimg.com/originals/90/40/03/9040034f5d635f46a4fb92128964fcca.jpg", "https://hips.hearstapps.com/hmg-prod/images/hummingbird-feeding-at-bleeding-heart-bloom-royalty-free-image-1656108706.jpg?crop=0.536xw:1.00xh;0.116xw,0&resize=980:*", 
-  // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAueXA2PdI6l2P6O2xe0RU5DP2ZWu5QmZ_Xg&s",
-  // "https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_640.jpg",
   "https://hips.hearstapps.com/hmg-prod/images/surprising-flower-meanings-balloon-flowers-1650767465.jpg?crop=1xw:1xh;center,top&resize=980:*",
   "https://cdn.pixabay.com/photo/2024/03/15/18/53/magnolia-flower-8635583_640.jpg",
   "https://cdn.pixabay.com/photo/2022/09/01/04/40/anemone-7424709_640.jpg",
@@ -27,12 +38,12 @@ const lightBackgroundImageArray = ["https://cdn.pixabay.com/photo/2023/03/26/07/
   "https://cdn.pixabay.com/photo/2024/03/15/18/53/magnolia-flower-8635583_640.jpg",
 ]
 
-const { text } = require('body-parser');
-const { Router } = require('express');
 
-const router = Router();
 
-router.get('/', (req, res) => res.render('pages/index', {messages: messages, title: "Leave nothing unsaid"}));
+router.get('/', (req, res) => {
+  messages = loadMessages();
+  res.render('pages/index', {messages: messages, title: "Leave nothing unsaid"})
+});
 // router.get('/new', (req, res) => res.render('pages/new'));
 
 // router.get('/new', (req, res) => {
@@ -62,6 +73,8 @@ router.post('/new', (req, res) => {
   }
   {messages.unshift({ text: messageText, user: messageUser, added: new Date(), backgroundImageArray: backgroundImage, 
     lightnessCheck: lightnessCheck, fromTextColor: fromTextColor, messageTextColor: messageTextColor});}
+
+  saveMessages(messages);
   res.redirect('/');
 });
 
